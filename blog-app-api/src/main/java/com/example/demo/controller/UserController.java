54404660f29ffb0b8be40caf1c1f1571a94e1848
CHANDAVALE Saurabh <saurabhchandavale@gmail.com>
2024-05-31 20:40:25 +0530

    commit

--- blog-app-api/src/main/java/com/example/demo/controller/UserController.java
+++ blog-app-api/src/main/java/com/example/demo/controller/UserController.java
@@ -1,14 +1,21 @@
 package com.example.demo.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.payload.ApiResponse;
 import com.example.demo.payload.UserDto;
 import com.example.demo.services.UserService;
 
@@ -22,11 +29,34 @@ public class UserController {
 	//POST-Create
 	@PostMapping("/create")
 	public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto){
-	
-
 		
 		UserDto createUserDto = this.userService.createUser(userDto);
 		return new ResponseEntity<>(createUserDto,HttpStatus.CREATED);
 	}
+	@PutMapping("/{userId}")
+	public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto, @PathVariable Integer userId){
+		System.out.println("Print userid : " + userId);
+		UserDto updateUser = this.userService.updateUser(userDto, userId);
+		return ResponseEntity.ok(updateUser);
+	}
+	
+	@DeleteMapping("/{userId}")
+	public ResponseEntity<ApiResponse> deleteUser(@PathVariable Integer userId){
+		this.userService.deleteUser(userId);
+		return new ResponseEntity<ApiResponse>(new ApiResponse("User deleted successfully", true), HttpStatus.OK);
+		}
+	@GetMapping("/")
+	public ResponseEntity<List<UserDto>> getAllUsers(){
+		return ResponseEntity.ok(this.userService.getAllUsers());
+	}
+	
+	@GetMapping("/{userId}")
+	public ResponseEntity<UserDto> getUserById(@PathVariable Integer userId){
+		return ResponseEntity.ok(this.userService.getUserById(userId));
+	}
+	
+	
+	
+	
 
 }
--- blog-app-api/src/main/java/com/example/demo/exception/ResourceNotFoundException.java
+++ blog-app-api/src/main/java/com/example/demo/exception/ResourceNotFoundException.java
@@ -9,11 +9,11 @@ public class ResourceNotFoundException extends RuntimeException {
 
 	String resourceName;
 	String fieldName;
-    long fieldValue;
+    int fieldValue;
 	
 	
-	public ResourceNotFoundException(String resourceName, String fieldName, long fieldValue) {
-		super(String.format("%s not found with %s : %l", resourceName, fieldName, fieldValue));
+	public ResourceNotFoundException(String resourceName, String fieldName, int fieldValue) {
+		super(String.format("%s not found with %s : %s", resourceName, fieldName, fieldValue));
 		this.resourceName = resourceName;
 		this.fieldName = fieldName;
 		this.fieldValue = fieldValue;
